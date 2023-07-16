d5b961a9ec907be02bfc20f85c61eafb83700f12
Derek Woodard <woodard.derek92@gmail.com>
2023-07-16 16:24:38 -0300

    added newline in day 1

--- Day 1 code - band name generator.py
+++ Day 1 code - band name generator.py
@@ -2,9 +2,9 @@
 def gen_band_name():
     print('Welcome to the Band Name Generator.')
     # Ask about the city the user grew up in - store the input as a variable
-    city = input("What's the name of the city you grew up in? ")
+    city = input("What's the name of the city you grew up in? \n")
     # Ask about their pet's name - store the input as a variable
-    pet = input("What's your pet's name? ")
-    print("Your band name could be", city, pet)
+    pet = input("What's your pet's name? \n")
+    print("Your band name could be", city, pet + "\n")
 
 gen_band_name()
