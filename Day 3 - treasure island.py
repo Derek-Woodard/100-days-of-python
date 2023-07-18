7b188c0610bd13842ed76bb3458e05d2e1a3d22c
Derek Woodard <woodard.derek92@gmail.com>
2023-07-18 15:33:20 -0300

    Completed Day 3 project

---
+++ Day 3 - treasure island.py
@@ -0,0 +1,58 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+
+# Take first input
+step1 = input('left or right? ').lower()
+
+# only left leads to game continue, so everything else is a game over
+if step1 != 'left':
+    print('Fall into a hole.\n Game Over.')
+    # on game over, exit the program
+    exit()
+
+step2 = input('swim or wait ').lower()
+
+if step2 != 'wait':
+    print('Attacked by trout.\n Game Over.')
+    exit()
+
+step3 = input('Which door? ').lower()
+
+if step3 == 'red':
+    print('Burned by fire.\n Game Over.')
+    exit()
+
+if step3 == 'blue':
+    print('Eaten by beasts.\n Game Over.')
+    exit()
+
+if step3 != 'yellow':
+    print('Game Over.')
+    exit()
+print('You Win!')
