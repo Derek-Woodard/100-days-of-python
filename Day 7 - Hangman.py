a83b44dedeee03dea8306190415407b325d4dae9
Derek Woodard <woodard.derek92@gmail.com>
2023-08-23 14:01:19 -0300

    added art for hangman. Completed game project

--- Day 7 - Hangman.py
+++ Day 7 - Hangman.py
@@ -11,11 +11,68 @@ print("""\
                    |___/  
 """)
 
+hangman = ["""\
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========""",
+"""\
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========""",
+"""\
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========""",
+"""\
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========""",
+"""\
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========""",
+"""\
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========""",
+"""\
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========""",]
+
 # Set up a list of words that can be used as solutions
 word_list = ["ardvark", "baboon", "camel"]
 
 # Set up number of lives that user has to lose
-lives = 6
+lives_lost = 0
 
 # Pick random word for the user to guess from the word list
 chosen_word = random.choice(word_list)
@@ -26,13 +83,7 @@ for i in chosen_word:
     blanks.append("_")
 
 # Start game loop
-while lives > 0:
-    # print the current ASCI art of the hangman
-
-
-    # Print the current blanks and guessed letters
-    print(blanks)
-
+while lives_lost < 6:
     # Take user input of one letter - force lower case
     guess = input("Guess a letter: ").lower()
 
@@ -43,16 +94,20 @@ while lives > 0:
             blanks[position] = guess
             guess_in_word = True
     
-    if guess_in_word:
-        print(f"You guessed {guess}. That is in the word")
-    else:
+    if not guess_in_word:
         print(f"You guessed {guess}. That is not in the word. You lose a life.")
-        lives -= 1
+        lives_lost += 1
+
+    # Print the current blanks and guessed letters
+    print(blanks)
+    
+    # print the current ASCI art of the hangman
+    print(hangman[lives_lost])
 
     if "_" not in blanks:
         print(f"You Win! Your word was {chosen_word}")
         exit()
     
-    if lives == 0:
+    if lives_lost == 6:
         print("Game Over.")
         exit()
