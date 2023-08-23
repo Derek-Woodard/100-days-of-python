72291ff1d697bc925f8b2b26f7946fe3282972be
Derek Woodard <woodard.derek92@gmail.com>
2023-08-23 13:51:20 -0300

    completed game loop. added win and lose conditions

--- Day 7 - Hangman.py
+++ Day 7 - Hangman.py
@@ -11,9 +11,6 @@ print("""\
                    |___/  
 """)
 
-# Set up game over condition for loop
-game_over = False
-
 # Set up a list of words that can be used as solutions
 word_list = ["ardvark", "baboon", "camel"]
 
@@ -28,10 +25,8 @@ blanks = []
 for i in chosen_word:
     blanks.append("_")
 
-
-tester = 0
 # Start game loop
-while tester < 5: #not game_over:
+while lives > 0:
     # print the current ASCI art of the hangman
 
 
@@ -42,9 +37,22 @@ while tester < 5: #not game_over:
     guess = input("Guess a letter: ").lower()
 
     # Compare the user guessed letter to the random word
-    index = 0
-    for letter in chosen_word:
-        if letter == guess:
-            blanks[index] = guess
-        index += 1
-    tester += 1
+    guess_in_word = False
+    for position in range(len(chosen_word)):
+        if chosen_word[position] == guess:
+            blanks[position] = guess
+            guess_in_word = True
+    
+    if guess_in_word:
+        print(f"You guessed {guess}. That is in the word")
+    else:
+        print(f"You guessed {guess}. That is not in the word. You lose a life.")
+        lives -= 1
+
+    if "_" not in blanks:
+        print(f"You Win! Your word was {chosen_word}")
+        exit()
+    
+    if lives == 0:
+        print("Game Over.")
+        exit()
