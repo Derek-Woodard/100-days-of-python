ab00a32f08e3820d74770816b90c83285ff02c74
Derek Woodard <woodard.derek92@gmail.com>
2023-08-24 15:39:33 -0300

    removed uneccessary double of alphabet

--- Day 8 - Caesar Cipher.py
+++ Day 8 - Caesar Cipher.py
@@ -1,6 +1,6 @@
 from cipher_art import logo
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 print(logo)
 
