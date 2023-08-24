5cb7a3df9889ab8f37d9fb11e5f1982ed570f898
Derek Woodard <woodard.derek92@gmail.com>
2023-08-24 15:38:50 -0300

    added loop and symbol recognition

--- Day 8 - Caesar Cipher.py
+++ Day 8 - Caesar Cipher.py
@@ -7,27 +7,40 @@ print(logo)
 def caesar(message, shift, direction):
     new_message = ''
     for index in range(len(message)):
-        letter_index = alphabet.index(message[index])
+        # Check if the character is a letter in the alphabet
+        if message[index] in alphabet:
+            letter_index = alphabet.index(message[index])
 
-        # pick direction of shift based on encode or decode
-        if direction == 'encode':
-            new_index = letter_index + shift
+            # pick direction of shift based on encode or decode
+            if direction == 'encode':
+                new_index = letter_index + shift
+            else:
+                new_index = letter_index - shift
+
+            # wrap around if index goes out of bounds of alphabet list
+            if new_index > len(alphabet):
+                new_index -= len(alphabet)
+            if new_index < 0:
+                new_index += len(alphabet)
+            new_message += alphabet[new_index]
+
+        # If the character is not in the alphabet, simply put the symbol in the encrypted message
         else:
-            new_index = letter_index - shift
+            new_message += message[index]
 
-        # wrap around if index goes out of bounds of alphabet list
-        if new_index > len(alphabet):
-            new_index -= len(alphabet)
-        if new_index < 0:
-            new_index += len(alphabet)
-
-        new_message += alphabet[new_index]
     return new_message
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: \n").lower()
-message = input("Type your message: \n").lower()
-shift = int(input("Type the shift number: \n"))
+# variable for repeating the loop
+repeat = 'yes'
+
+# Loop as long as the user wants to
+while repeat == 'yes':
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: \n").lower()
+    message = input("Type your message: \n").lower()
+    shift = int(input("Type the shift number: \n"))
 
 
-encrypted_message = caesar(message, shift, direction)
-print(f"Your new message is: {encrypted_message}")
+    encrypted_message = caesar(message, shift, direction)
+    print(f"Your new message is: {encrypted_message}")
+
+    repeat = input("Do you want to continue? 'yes' or 'no'\n").lower()
