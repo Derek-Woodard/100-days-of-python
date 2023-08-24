26abe7ab5e606dbbfbb4aeef07eeeae49f88d187
Derek Woodard <woodard.derek92@gmail.com>
2023-08-24 15:29:24 -0300

    completed base encrypt/decrypt of letters

--- Day 8 - Caesar Cipher.py
+++ Day 8 - Caesar Cipher.py
@@ -4,21 +4,30 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 print(logo)
 
-def encrypt(message, shift):
-    new_message = message
+def caesar(message, shift, direction):
+    new_message = ''
     for index in range(len(message)):
         letter_index = alphabet.index(message[index])
-        new_index = letter_index + int(shift)
+
+        # pick direction of shift based on encode or decode
+        if direction == 'encode':
+            new_index = letter_index + shift
+        else:
+            new_index = letter_index - shift
+
+        # wrap around if index goes out of bounds of alphabet list
         if new_index > len(alphabet):
             new_index -= len(alphabet)
-        new_message[index] == alphabet[new_index]
+        if new_index < 0:
+            new_index += len(alphabet)
+
+        new_message += alphabet[new_index]
     return new_message
 
-encode = input("Type 'encode' to encrypt, type 'decode' to decrypt: \n").lower()
-message = input("Type your message: \n")
-shift = input("Type the shift number: \n")
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: \n").lower()
+message = input("Type your message: \n").lower()
+shift = int(input("Type the shift number: \n"))
 
-if encode == 'encode':
-    new_message = encrypt(message, shift)
-    print(f"Your new message is: {new_message}")
-#elif encode == 'decode':
+
+encrypted_message = caesar(message, shift, direction)
+print(f"Your new message is: {encrypted_message}")
