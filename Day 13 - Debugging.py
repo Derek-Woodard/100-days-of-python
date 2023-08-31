58a8c377a980b603c9d293fa2e41dc23d49d673c
Derek Woodard <woodard.derek92@gmail.com>
2023-08-31 16:21:36 -0300

    added comments on debugging steps/tips

--- Day 13 - Debugging.py
+++ Day 13 - Debugging.py
@@ -1,1 +1,9 @@
 # Debugging learned through interactive course in 100 days of python program
+
+# debugging steps
+# 1) Describe the bug
+# 2) Reproduce the bug - try to force the situation that makes the bug occur - sometimes random
+# 3) Play Computer - Think like the computer, ONLY consider the information the computer is given
+# 4) Fix the error - 
+# 5) Print is your friend - use print statements to track the values of variables that are potentiall causing problems
+# 6) Use a debugger - put break points into the code around the problem area and step through the code using a debugger
