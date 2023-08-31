f28bd243726b9a57689d2ee7e83f029132ead945
Derek Woodard <woodard.derek92@gmail.com>
2023-08-31 16:22:32 -0300

    described fix the error

--- Day 13 - Debugging.py
+++ Day 13 - Debugging.py
@@ -4,6 +4,6 @@
 # 1) Describe the bug
 # 2) Reproduce the bug - try to force the situation that makes the bug occur - sometimes random
 # 3) Play Computer - Think like the computer, ONLY consider the information the computer is given
-# 4) Fix the error - 
+# 4) Fix the error - When an error is shown, either while coding or when running, fix it immediately.
 # 5) Print is your friend - use print statements to track the values of variables that are potentiall causing problems
 # 6) Use a debugger - put break points into the code around the problem area and step through the code using a debugger
