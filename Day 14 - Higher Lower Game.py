b499c3b03067ddc8b8a336dabb549ac01030c0e4
Derek Woodard <woodard.derek92@gmail.com>
2023-09-01 11:24:40 -0300

    listed flow of game in comments

--- Day 14 - Higher Lower Game.py
+++ Day 14 - Higher Lower Game.py
@@ -3,3 +3,20 @@ from higher_lower_art import logo, vs
 import random
 import os
 
+playing = True
+
+while playing:
+    print(logo)
+
+    # pick two random selections from the data list.
+    # display their information. ex:
+    # Compare A: Taylor Swift, a Musician, from United States.
+    # print the vs art
+    # then print the second choice
+    # remove the two selected choices from the list?
+    # Ask "Who has more followers? Type 'A' of 'B': "
+    # player inputs - compare the follower amount of the selections
+    # if correct, add 1 to score, then clear screen, show the score and display a  new comparison
+    # if incorrect, display a final score and ask if they want to play again.
+
+    
