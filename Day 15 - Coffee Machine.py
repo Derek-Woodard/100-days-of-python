e8840e88f108a31b415da3091e7bb1886db291ac
Derek Woodard <woodard.derek92@gmail.com>
2023-09-01 14:42:30 -0300

    added menu, resources and to-do list

---
+++ Day 15 - Coffee Machine.py
@@ -0,0 +1,43 @@
+
+
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+# Start by asking what the user would like, espresso/latte/cappuccino
+# tell user to insert coins
+# ask for quarters, then dimes, then nickels, then pennies.
+# if the coins add up to more than the cost of the drink, tell the user what they get back in change
+# Then give them their coffee using an emoji as well.
+# if the coins don't add up to the cost, refund the user then ask what they would like again.
+# track resources in the machine - each drinks takes a certain amount
+# if a drink requires more foa  resource than is available, say there is not enough, then ask for what drink again.
+# to do more, add funtcionality to refill the resources?
