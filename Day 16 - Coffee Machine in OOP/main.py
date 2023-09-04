57f50bf42f324057ddcd21cd5a466d3a5a78cc67
Derek Woodard <woodard.derek92@gmail.com>
2023-09-04 12:04:18 -0300

    take order, show report, can turn off

--- Day 16 - Coffee Machine in OOP/main.py
+++ Day 16 - Coffee Machine in OOP/main.py
@@ -2,3 +2,22 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+cm = CoffeeMaker()
+menu = Menu()
+mm = MoneyMachine()
+
+ordering = True
+
+while ordering:
+    choice = input("What would you like? (espresso/latte/cappucinno)")
+
+    if choice == 'report':
+        cm.report()
+        continue
+
+    if choice == 'off':
+        break
+
+    drink = menu.find_drink(choice)
+    
+    
