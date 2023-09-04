10fa55f91d05f062aa859a144c019e5ccf3620f5
Derek Woodard <woodard.derek92@gmail.com>
2023-09-04 12:27:46 -0300

    completed Day 16 project

--- Day 16 - Coffee Machine in OOP/main.py
+++ Day 16 - Coffee Machine in OOP/main.py
@@ -1,3 +1,4 @@
+'''A program that imitates a coffee machine using Object Oriented Programming (OOP)'''
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
@@ -13,11 +14,17 @@ while ordering:
 
     if choice == 'report':
         cm.report()
+        mm.report()
         continue
 
     if choice == 'off':
         break
 
     drink = menu.find_drink(choice)
-    
-    
+
+    if drink is None:
+        continue
+
+    if cm.is_resource_sufficient(drink):
+        if mm.make_payment(drink.cost):
+            cm.make_coffee(drink)
