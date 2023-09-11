6591c172a79731267c1da309c8797ec677e1d02e
Derek Woodard <woodard.derek92@gmail.com>
2023-09-11 10:38:22 -0300

    added reset to etch-a-sketch. changed distances

--- Day 19 - Turtle Race/Etch_A_Sketch.py
+++ Day 19 - Turtle Race/Etch_A_Sketch.py
@@ -6,27 +6,31 @@ screen = Screen()
 
 def forward():
     '''move the turtle forwards.'''
-    etch.forward(5)
+    etch.forward(10)
 
 def left():
     '''Turn the turtle left'''
-    etch.left(5)
+    etch.left(10)
 
 def right():
     '''Turn the turtle right.'''
-    etch.right(5)
+    etch.right(10)
 
 def back():
     '''move the turtle backwards.'''
-    etch.back(5)
+    etch.back(10)
 
-# note that the function onkey takes another function as input
+def reset():
+    '''clear the screen and put restart at the center.'''
+    etch.reset()
+
+# note that the function onkey takes another function as input - higher order functions
 # the input function has no (), as they call the function
 screen.listen()
 screen.onkey(forward, 'w')
 screen.onkey(left, 'a')
 screen.onkey(right, 'd')
 screen.onkey(back, 's')
-
+screen.onkey(reset, 'c')
 
 screen.exitonclick()
