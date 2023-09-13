08b4705c555b05f6ba7dc15a9945756d131cda50
Derek Woodard <woodard.derek92@gmail.com>
2023-09-13 08:53:09 -0300

    made snake todo list

---
+++ Day 20 - Snake Game/main.py
@@ -0,0 +1,9 @@
+'''Snake game using OOP and the turtle module'''
+#TODO break game down into 7 steps - first 3 on day 20, last 4 on day 21
+#TODO Step 1: create the snake body - uise 3 squares
+#TODO Step 2: move the snake - always moving forward - can change direction
+#TODO Step 3: control the snake - use arrow keys
+#TODO Step 4: detect collision for food - add food, and make snake grow
+#TODO Step 5: create scoreboard - track number of food pieces
+#TODO Step 6: detect wall collisions - game over screen
+#TODO Step 7: detect collision with tail - game over screen
