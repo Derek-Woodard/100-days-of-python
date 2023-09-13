3696be753803c4c79be1edbd3d8e450aefbbc095
Derek Woodard <woodard.derek92@gmail.com>
2023-09-13 11:37:46 -0300

    added snake forward movement

--- Day 20 - Snake Game/main.py
+++ Day 20 - Snake Game/main.py
@@ -1,6 +1,6 @@
 '''Snake game using OOP and the turtle module'''
-#TODO break game down into 7 steps - first 3 on day 20, last 4 on day 21
-#TODO Step 1: create the snake body - uise 3 squares
+# break game down into 7 steps - first 3 on day 20, last 4 on day 21
+# Step 1: create the snake body - uise 3 squares
 #TODO Step 2: move the snake - always moving forward - can change direction
 #TODO Step 3: control the snake - use arrow keys
 #TODO Step 4: detect collision for food - add food, and make snake grow
@@ -8,6 +8,7 @@
 #TODO Step 6: detect wall collisions - game over screen
 #TODO Step 7: detect collision with tail - game over screen
 
+import time
 from turtle import Turtle, Screen
 from snake_body import Snake
 
@@ -15,8 +16,21 @@ screen = Screen()
 screen.setup(width=600,height=600)
 screen.bgcolor("black")
 screen.title("Snake using turtles")
+screen.tracer(0)
 
 player = Snake()
 
+playing = True
+temp = 0
+
+#while playing:
+while temp < 40:
+    # screen.listen()
+    # screen.onkeypress(,"up")
+
+    time.sleep(0.1)
+    screen.update()
+    player.move_snake()
+    temp += 1
 
 screen.exitonclick()
--- Day 20 - Snake Game/snake_body.py
+++ Day 20 - Snake Game/snake_body.py
@@ -3,10 +3,33 @@
 from turtle import Turtle
 
 class Snake():
+    '''The snake class that tracks the number of segments
+    This also tracks the location of each segment
+    '''
     def __init__(self):
         self.segments = []
         for i in range(3):
             segment = Turtle("square")
             segment.color("white")
+            segment.penup()
             self.segments.append(segment)
             segment.setpos(i*-20, 0)
+
+    def move_snake(self):
+        for seg in self.segments:   
+            seg.forward(20)
+
+            # if self.segments.index(seg) == 0:
+            #     temp_loc = seg.pos()
+            #     seg.forward(3)
+            # else:
+            #     movement = temp_loc - seg.pos()
+            #     print(temp_loc)
+            #     print(seg.pos())
+            #     print(f"{movement} segment {self.segments.index(seg)}")
+            #     seg.goto(movement)
+            #     temp_loc = seg.pos()
+                
+
+
+    # def turn_snake(self, direction):
