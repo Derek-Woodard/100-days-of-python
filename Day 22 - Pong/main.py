224ece849e920a96a6dd832bbae5b3c77e0c8291
Derek Woodard <woodard.derek92@gmail.com>
2023-09-14 16:28:23 -0300

    Made to-do list for pong

---
+++ Day 22 - Pong/main.py
@@ -0,0 +1,13 @@
+'''
+The Pong game created using turtles
+'''
+
+#TODO break down the game into steps
+#TODO Step 0.5) Set up the screen with the centre line
+#TODO Step 1) create a paddle class that displays paddles on the screen
+#TODO Step 2) allow user input to move the paddles up/down - not leaving the screen
+#TODO Step 3) Create a ball class that can spawn a ball that moves
+#TODO Step 4) Give the ball collision with the top/bottom to bounce
+#TODO Step 5) Give the ball collision with the left/right to score then respawn
+#TODO Step 6) Give the ball collision with the paddles to bounce
+#TODO Step 7) Create a scoreboard class that can track/display the score values
