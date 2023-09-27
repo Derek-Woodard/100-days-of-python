8d4965b960d1400b820679ff13ff138599fd11f5
Derek Woodard <woodard.derek92@gmail.com>
2023-09-27 14:53:27 -0300

    Set up starting files and text files

---
+++ Day 24 - Mail Merge/main.py
@@ -0,0 +1,7 @@
+'''take a list of names and insert them into a letter'''
+
+#TODO make a list of names in a text file called invited_guests.txt
+#TODO make a default letter script
+#TODO Access the names to write letter to each person using script
+#TODO save the letters to another folder
+
