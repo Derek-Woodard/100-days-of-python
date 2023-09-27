5d91af06a0a76ce9ceb2be0c0b70b0078cec5bdc
Derek Woodard <woodard.derek92@gmail.com>
2023-09-27 15:04:41 -0300

    reading template and names from files

--- Day 24 - Mail Merge/main.py
+++ Day 24 - Mail Merge/main.py
@@ -5,3 +5,17 @@
 #TODO Access the names to write letter to each person using script
 #TODO save the letters to another folder
 
+# use the following: readlines(), replace(), strip() for strings
+
+with open('Day 24 - Mail Merge/Input/Letters/starting_letter.txt') as base_template:
+    template = base_template.read()
+    print(template)
+
+TEMP = "\n"
+
+with open('Day 24 - Mail Merge/Input/Names/invited_names.txt') as names:
+    while TEMP:
+        TEMP = names.readline()
+        TEMP.strip()
+        print(TEMP)
+        
