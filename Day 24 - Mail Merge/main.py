55ac9e0648099f70d3ae63fcbd4bff3fe99f42e6
Derek Woodard <woodard.derek92@gmail.com>
2023-09-27 15:25:46 -0300

    read from script and names save as letters to each

--- Day 24 - Mail Merge/main.py
+++ Day 24 - Mail Merge/main.py
@@ -1,21 +1,23 @@
 '''take a list of names and insert them into a letter'''
 
-#TODO make a list of names in a text file called invited_guests.txt
-#TODO make a default letter script
-#TODO Access the names to write letter to each person using script
-#TODO save the letters to another folder
+# make a list of names in a text file called invited_guests.txt
+# make a default letter script
+# Access the names to write letter to each person using script
+# save the letters to another folder
 
 # use the following: readlines(), replace(), strip() for strings
 
 with open('Day 24 - Mail Merge/Input/Letters/starting_letter.txt') as base_template:
     template = base_template.read()
-    print(template)
 
 TEMP = "\n"
 
 with open('Day 24 - Mail Merge/Input/Names/invited_names.txt') as names:
     while TEMP:
-        TEMP = names.readline()
-        TEMP.strip()
-        print(TEMP)
-        
+        TEMP = names.readline().strip()
+        if TEMP != "":
+            new_letter = template.replace("[name]", TEMP)
+            letter_name = f"Letter_for_{TEMP}.txt"
+
+            with open(f"Day 24 - Mail Merge/Output/ReadyToSend/{letter_name}", "w") as output_letter:
+                output_letter.write(new_letter)
