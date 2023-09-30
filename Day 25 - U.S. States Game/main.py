39af67ff445e2c2e22e8d2171ef3cd3e4cd3d6b9
Derek Woodard <woodard.derek92@gmail.com>
2023-09-30 16:00:07 -0300

    added title case to input

--- Day 25 - U.S. States Game/main.py
+++ Day 25 - U.S. States Game/main.py
@@ -18,21 +18,20 @@ known_list = []
 dot_marker = Turtle(visible=False)
 dot_marker.penup()
 
-while QUIZ_ACTIVE:
-    guess = turtle.textinput("U.S. States", "Name any U.S. State - type 'quit' to exit")
+while len(known_list) < 50 and QUIZ_ACTIVE:
+    guess = (turtle.textinput(f"{len(known_list)}/50", "Name any U.S. State - type 'quit' to exit")).title()
 
     if guess in state_list and guess not in known_list:
-        
+
         state_row = data[data.state == guess]
         dot_marker.goto(int(state_row.x), int(state_row.y))
-        dot_marker.dot(5, "blue")
-        dot_marker.setheading(90)
-        dot_marker.forward(10)
         dot_marker.write(f'{guess}',False,'left',("Times New Roman", 8, "normal"))
 
         known_list.append(guess)
 
-    if guess == "quit":
+    if guess == "Quit":
         QUIZ_ACTIVE = False
 
-screen.exitonclick()
+
+
+turtle.mainloop()
