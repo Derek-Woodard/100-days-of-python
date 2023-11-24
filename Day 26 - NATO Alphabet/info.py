c71e7e517e6d19095d32fb037a910061c7acb5f1
Derek Woodard <woodard.derek92@gmail.com>
2023-11-24 12:11:13 -0500

    made info file

---
+++ Day 26 - NATO Alphabet/info.py
@@ -0,0 +1,7 @@
+'''
+Lists and Dictionary comprehension
+Aim to make a console program where you enter a word in console
+That word is then returned as a list giving the NATO words for each letter
+
+
+'''
