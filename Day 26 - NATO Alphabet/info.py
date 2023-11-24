6b2757ddb6d9df8d50d71d40f6c4da892f644a2a
Derek Woodard <woodard.derek92@gmail.com>
2023-11-24 12:45:28 -0500

    more list_comprehension examples

--- Day 26 - NATO Alphabet/info.py
+++ Day 26 - NATO Alphabet/info.py
@@ -27,4 +27,28 @@ new_list = [n + 1 for n in numbers]
 
 numbers = [1,2,3]
 new_numbers = [n+1 for n in numbers]
-print(new_numbers)
+# print(new_numbers)
+
+# working with a string
+NAME = 'Derek'
+# takes each letter in a string and adds it into the list
+letters_list = [letter for letter in NAME]
+# print(letters_list)
+
+# challenge:
+# create a range from 1-5 using range(1,5) that doubles the values:
+range_list = [number * 2 for number in range(1,5)]
+print(range_list)
+
+# conditional list comprehension: new_list = [n + 1 for n in numbers if test]
+# if test allows us to only add new_item if test passes
+# ex:
+names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+# make a new list that only has the names with 4 or fewer letters
+short_names = [name for name in names if len(name) < 5]
+print(short_names)
+
+# challenge
+# take above list of names and any name with 5+ letters gets turned into the upper case version
+long_names = [name.upper() for name in names if len(name) > 4]
+print(long_names)
